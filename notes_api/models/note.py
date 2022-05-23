0dc704710a75a0c276f107ac2e254e87ce95067f
Nicolas Thuillier <nicothll.dev@gmail.com>
2022-05-22 19:01:44 -0500

    deleted Category model and modified Note routes

--- notes_api/models/note.py
+++ notes_api/models/note.py
@@ -1,21 +1,11 @@
 from typing import Optional
-from pydantic import BaseModel
 from beanie import Document
-
-
-class Category(BaseModel):
-    name: str
-    description: Optional[str]
-
-    class Settings:
-        name = "categories"
 
 
 class Note(Document):
     title: str
     description: str
-    checked: bool
-    category: Optional[Category]
+    checked: bool = False
 
     class Settings:
         name = "notes"
--- notes_api/routes/note.py
+++ notes_api/routes/note.py
@@ -1,7 +1,6 @@
-from pprint import pprint
 from fastapi import APIRouter, Depends, Response
 
-from notes_api.models.note import Note, Category
+from notes_api.models.note import Note
 
 router = APIRouter(prefix="/notes", tags=["Note"])
 
@@ -9,11 +8,29 @@ router = APIRouter(prefix="/notes", tags=["Note"])
 @router.get("")
 async def fetch_all_notes():
     response = await Note.find_all().to_list()
-    pprint(response)
-
     return response
+
+
+@router.post("")
+async def create_note(note: Note):
+    response = await note.insert()
+    return Response(status_code=201, content=str(response))
 
 
 @router.get("/{id}", response_model=Note)
 async def fetch_one_note(id: str):
     return await Note.get(id)
+
+
+@router.put("/{id}", response_model=Note)
+async def update_note(id: str, data: dict):
+    note = await Note.get(id)
+    await note.set(data)
+    return note
+
+
+@router.delete("/{id}", response_model=Note)
+async def remove_note(id: str):
+    note = await Note.get(id)
+    await note.delete()
+    return Response(status_code=204)
